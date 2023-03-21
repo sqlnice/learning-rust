a6e05c65cb96f3db30dfdcbd62c5531da9a84a86
sqlnice <472252510@qq.com>
2023-03-21 22:42:00 +0800

    📦 NEW: 3.2 数据类型

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use rand::Rng;
 use std::{cmp::Ordering, io};
 fn main() {
     // guess_number()
-    variables_and_mutability()
+    // variables_and_mutability()
+    data_types()
 }
 
 // 2 猜数字游戏
@@ -63,4 +64,48 @@ fn variables_and_mutability() {
     let spaces = "      ";
     let spaces = spaces.len();
     println!("spaces的长度为:{spaces}");
+}
+// 3.2 数据类型
+fn data_types() {
+    // 标量类型（scalar）代表一个单独的值 整形、浮点型、布尔类型、字符类型
+
+    // - 整形 无小数部分 默认 i32
+    // - 浮点型 f32 f64；默认 f64； 所有浮点型都是有符号的；浮点数采用IEEE-754标准
+    let x = 2.0;
+    let y: f32 = 3.0;
+    println!("{x}{y}");
+    // - 数值运算 支持加减乘除和取余；整数除法会向下舍入到最接近的整数
+    // - 字符类型
+    // 使用 '' 声明 char 字面量；使用 "" 声明字符串字面量；
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // 复合类型（Compound types） 可以将多个值组合成一个类型
+    // - 元祖类型 一旦声明，长度不会变化
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    // 使用模式匹配来解构
+    let (x, y, z) = tup;
+    println!("{x}{y}{z}");
+    // 使用 . 直接访问
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    // - 数组类型 每个元素必须相同；长度固定
+    let months: [&str; 12] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a = [3; 5]; // 生成 [3, 3, 3, 3, 3]
+    let first = a[0]; // 使用索引访问
 }
