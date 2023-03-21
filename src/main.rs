7f95f6b3fcf332a8015c4e83abfc408c5562e713
sqlnice <472252510@qq.com>
2023-03-21 22:07:00 +0800

    📦 NEW: 2.猜数字游戏

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
 fn main() {
-    println!("Hello, world!");
+    guess_number()
+}
+
+// 2 猜数字游戏
+fn guess_number() {
+    println!("猜数字游戏!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("请输入你猜测的数字");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("读取错误");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你猜测的数字是：{guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小啦"),
+            Ordering::Greater => println!("太大啦"),
+            Ordering::Equal => {
+                println!("你赢啦");
+                break;
+            }
+        }
+    }
 }
