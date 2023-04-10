c4af7bd95b55cf0c60051d22612ad7fd695bbf5e
sqlnice <472252510@qq.com>
2023-04-10 22:18:01 +0800

    📦 NEW: 10.3 使用生命周期来确保引用有效

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,8 @@ fn main() {
     // hash_maps()
     // unrecoverable_errors_with_panic()
     // recoverable_errors_with_result()
-    traits()
+    // traits()
+    lifetime_syntax()
 }
 // 2 猜数字游戏
 fn guess_number() {
@@ -930,4 +931,85 @@ fn traits() {
         }
     }
     // trait 和 trait bound 让我们使用泛型类型参数来减少重复,并仍然能够向编译器明确指定泛型类型需要拥有哪些行为.因为我们向编译器提供了 trait bound 信息，它就可以检查代码中所用到的具体类型是否提供了正确的行为。在动态类型语言中，如果我们尝试调用一个类型并没有实现的方法，会在运行时出现错误。Rust 将这些错误移动到了编译时，甚至在代码能够运行之前就强迫我们修复错误。另外，我们也无需编写运行时检查行为的代码，因为在编译时就已经检查过了，这样相比其他那些不愿放弃泛型灵活性的语言有更好的性能。
+}
+// 10.3 使用生命周期来确保引用有效
+fn lifetime_syntax() {
+    // 大部分生命周期是隐含并可以推断的;
+    // 生命周期避免了悬垂引用
+    // let r;
+    // {
+    //     let x = 5;
+    //     r = &x;
+    // }
+    // println!("r: {r}"); // 此时 r 引用的就是 x 最后被释放的内存
+
+    // 借用检查器 borrow checker; 它比较作用域来确保所有的借用都是有效的
+
+    // 函数中的泛型生命周期
+    // 泛型生命周期 'a 的具体生命周期等同于 x 和 y 的生命周期中较小的那一个
+    // fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    //     if x.len() > y.len() {
+    //         x
+    //     } else {
+    //         y
+    //     }
+    // }
+    // let string1 = String::from("abcd");
+    // {
+    //     let string2 = "xyz";
+    //     let result = longest(string1.as_str(), string2);
+    //     println!("The longest string is {}", result);
+    // }
+
+    // 指定返回参数的生命周期
+    fn longest<'a>(x: &'a str, y: &str) -> &'a str {
+        x
+    }
+
+    // 生命周期语法是用于将函数的多个参数与其返回值的生命周期进行关联的。一旦他们形成了某种关联，Rust 就有了足够的信息来允许内存安全的操作并阻止会产生悬垂指针亦或是违反内存安全的行为
+
+    // 结构体定义中的生命周期注解
+    // 这个注解意味着 ImportantExcerpt 的实例不能比其 part 字段中的引用存在的更久
+    struct ImportantExcerpt<'a> {
+        part: &'a str,
+    }
+    let novel = "Call me Ishmael. Some years ago...".to_string();
+    let first_sentence = novel.split(".").next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    // 生命周期省略
+    // 被编码进 Rust 引用分析的模式被称为 生命周期省略规则（lifetime elision rules）。这并不是需要程序员遵守的规则；这些规则是一系列特定的场景，此时编译器会考虑，如果代码符合这些场景，就无需明确指定生命周期。
+    // 函数或方法的参数的生命周期被称为 输入生命周期（input lifetimes），而返回值的生命周期被称为 输出生命周期（output lifetimes）。
+    // 编译器采用三条规则来判断引用何时不需要明确的注解。第一条规则适用于输入生命周期，后两条规则适用于输出生命周期。如果编译器检查完这三条规则后仍然存在没有计算出生命周期的引用，编译器将会停止并生成错误。这些规则适用于 fn 定义，以及 impl 块。
+    // 第一条规则是编译器为每一个引用参数都分配一个生命周期参数。换句话说就是，函数有一个引用参数的就有一个生命周期参数：fn foo<'a>(x: &'a i32)，有两个引用参数的函数就有两个不同的生命周期参数，fn foo<'a, 'b>(x: &'a i32, y: &'b i32)，依此类推。
+    // 第二条规则是如果只有一个输入生命周期参数，那么它被赋予所有输出生命周期参数：fn foo<'a>(x: &'a i32) -> &'a i32。
+    // 第三条规则是如果方法有多个输入生命周期参数并且其中一个参数是 &self 或 &mut self，说明是个对象的方法 (method)(译者注：这里涉及 rust 的面向对象参见 17 章)，那么所有输出生命周期参数被赋予 self 的生命周期。第三条规则使得方法更容易读写，因为只需更少的符号。只能适用于 方法签名
+
+    // 方法定义中的生命周期注解
+    // 这里有两个输入生命周期，所以 Rust 应用第一条生命周期省略规则并给予 &self 和 announcement 他们各自的生命周期。接着，因为其中一个参数是 &self，返回值类型被赋予了 &self 的生命周期，这样所有的生命周期都被计算出来了。
+    impl<'a> ImportantExcerpt<'a> {
+        fn announce_and_return_part(&self, announcement: &str) -> &str {
+            println!("Attention please: {}", announcement);
+            self.part
+        }
+    }
+
+    // 静态生命周期; 所有的字符串字面值都拥有 'static 生命周期
+    let s: &'static str = "I have a static lifetime";
+    // 大部分情况中，推荐 'static 生命周期的错误信息都是尝试创建一个悬垂引用或者可用的生命周期不匹配的结果。在这种情况下的解决方案是修复这些问题而不是指定一个 'static 的生命周期。
+
+    // 结合泛型类型参数/trait bounds 和生命周期
+    fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where
+        T: Display,
+    {
+        println!("Announcement! {}", ann);
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
 }
