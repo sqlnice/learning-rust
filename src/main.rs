19a287fd194b82c09146650cd2bceee02eab14b5
时启龙 <shiqilong@beyondcent.com>
2023-05-01 13:03:49 +0800

    📦 NEW: 17.3 面向对象设计模式的实现

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,8 @@ fn main() {
     // reference_cycles()
     // threads()
     // message_passing()
-    shared_state()
+    // shared_state()
+    oo_design_patterns()
 }
 // 2 猜数字游戏
 fn guess_number() {
@@ -1739,4 +1740,82 @@ fn trait_objects() {
     };
     screen.run();
     // 使用 trait 对象和 Rust 类型系统来进行类似鸭子类型操作的优势是无需在运行时检查一个值是否实现了特定方法或者担心在调用时因为值没有实现方法而产生错误。如果值没有实现 trait 对象所需的 trait 则 Rust 不会编译这些代码。
+}
+
+// 17.3 面向对象设计模式的实现
+fn oo_design_patterns() {
+    // 状态模式（state pattern）是一个面向对象设计模式。该模式的关键在于定义一系列值的内含状态。这些状态体现为一系列的 状态对象，同时值的行为随着其内部状态而改变。
+
+    pub struct Post {
+        state: Option<Box<dyn State>>,
+        content: String,
+    }
+    impl Post {
+        pub fn new() -> Post {
+            Post {
+                state: Some(Box::new(Draft {})),
+                content: String::new(),
+            }
+        }
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text)
+        }
+        pub fn content(&self) -> &str {
+            self.state.as_ref().unwrap().content(self)
+        }
+        pub fn request_review(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.request_review())
+            }
+        }
+        pub fn approve(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.approve())
+            }
+        }
+    }
+    trait State {
+        fn request_review(self: Box<Self>) -> Box<dyn State>;
+        fn approve(self: Box<Self>) -> Box<dyn State>;
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            ""
+        }
+    }
+    struct Draft {}
+    impl State for Draft {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            Box::new(PendingReview {})
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+    }
+    struct PendingReview {}
+    impl State for PendingReview {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            Box::new(Published {})
+        }
+    }
+    struct Published {}
+    impl State for Published {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            &post.content
+        }
+    }
+    let mut post = Post::new();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+    post.request_review();
+    assert_eq!("", post.content());
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
